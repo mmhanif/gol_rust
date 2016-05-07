a2b4e910a9d25ac0218b53d7535b62967c8ac2fc
Mahmood Hanif <mmhanif@gmail.com>
2016-05-06 23:44:47 -0400

    Added timing to main

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,30 @@
 extern crate gol_rust;
 use std::collections::HashSet;
 
+#[macro_use]
+extern crate timeit;
+
 fn main() {
-    let v = vec![(25,25), (24,25), (24,26), (25, 24), (26, 25)];
-    let initial_state: HashSet<_> = v.iter().cloned().collect();
-    let next_state_1 = gol_rust::gol_1::next(&v);
-    let next_state_2 = gol_rust::gol_2::next(&initial_state);
-    for cell in next_state_1 {
-        println!("({},{})", cell.0, cell.1)
-    }
-    println!("-------------------------");
-    for cell in next_state_2 {
-        println!("({},{})", cell.0, cell.1)
-    }
+    let mut v = vec![(25,25), (24,25), (24,26), (25, 24), (26, 25)];
+    let mut state: HashSet<_> = v.iter().cloned().collect();
+
+    timeit! ({
+        for _ in 0..1000 {
+            v = gol_rust::gol_1::next(&v);
+        }
+    });
+
+    timeit! ({
+        for _ in 0..1000 {
+            state = gol_rust::gol_2::next(&state);
+        }
+    });
+
+    //for cell in v {
+    //    println!("({},{})", cell.0, cell.1)
+    //}
+    //println!("-------------------------");
+    //for cell in state {
+    //    println!("({},{})", cell.0, cell.1)
+    //}
 }
